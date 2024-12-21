6816cad5e94e9319449342e14c70bcae3fe95b64
Eric Andrews <50085784+iameveryman@users.noreply.github.com>
2024-12-21 16:47:38 -0500

    Add function getComputerChoice
    Added the getComputerChoice function to randomly
    return either 'rock', 'paper', or 'scissors'.

--- rockPaperScissors-script.js
+++ rockPaperScissors-script.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+function getComputerChoice(){
+    let compChoice = Math.random();
+    if (compChoice >= 0.667){
+        return 'rock'
+    }
+    else if (compChoice >= 0.334){
+        return 'paper'
+    }
+    else {
+        return 'scissors'
+    }
+};
+
+
+console.log(getComputerChoice());

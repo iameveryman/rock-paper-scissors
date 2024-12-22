94bbcb1fc0cf65c05ebb7040fde42b937df34011
Eric Andrews <50085784+iameveryman@users.noreply.github.com>
2024-12-22 10:30:37 -0500

    Add playRound function
    Began work on the playRound function.  So far it
    only defines tie scenario behavior.  More on the
    way.

--- rockPaperScissors-script.js
+++ rockPaperScissors-script.js
@@ -14,12 +14,24 @@ function getComputerChoice(){
     }
 };
 
-
-console.log(getComputerChoice());
-
 function getHumanChoice(string){
     let userChoice = prompt('What do you choose?', 'rock, paper, or scissors');
     return userChoice.toLowerCase();
 };
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice){
+    
+        //tie scenarios
+    if (humanSelection === computerSelection){
+        console.log('We have a tie! Try again')
+    }
+};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+    //trying to see each function's return
+// console.log(computerSelection);
+// console.log(humanSelection);
+
+console.log(playRound(humanSelection, computerSelection));

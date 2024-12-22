640e99a6809577d72342db8e1921cad0c330bff6
Eric Andrews <50085784+iameveryman@users.noreply.github.com>
2024-12-22 15:43:23 -0500

    Add logic for determining outcomes
    Added series of else if statements to determine
    whether humanChoice or computerChoice wins the
    round.

--- rockPaperScissors-script.js
+++ rockPaperScissors-script.js
@@ -20,18 +20,35 @@ function getHumanChoice(string){
 };
 
 function playRound(humanChoice, computerChoice){
+    const rockWins = ' Rock breaks scissors';
+    const scissorsWins = ' Scissors cut paper';
+    const paperWins = ' Paper covers rock';
+    const youLose = 'Oh no! ';
     
         //tie scenarios
     if (humanSelection === computerSelection){
         console.log('We have a tie! Try again')
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors'){
+        return console.log('You win!' + rockWins)
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper'){
+        return console.log('You win!' + scissorsWins)
+    } else if (humanChoice === 'paper' && computerChoice === 'rock'){
+        return console.log('You win!' + paperWins)
+    } else if (humanChoice === 'rock' && computerChoice === 'paper'){
+        return console.log(youLose + paperWins)
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors'){
+        return console.log(youLose + scissorsWins)
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock'){
+        return console.log(youLose + rockWins)
     }
+    
 };
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
+console.log(playRound(humanSelection, computerSelection));
+
     //trying to see each function's return
 // console.log(computerSelection);
 // console.log(humanSelection);
-
-console.log(playRound(humanSelection, computerSelection));
